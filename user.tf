723edbc53f0b4cd9887b41a94acc4a7b81dd2dd5
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-03-04 22:29:54 +0530

    Add files via upload

--- user.tf
+++ user.tf
@@ -6,3 +6,8 @@ resource "aws_iam_user" "shruti" {
   name = var.this_aws_iam_user_users[2]
   path = var.this_aws_iam_user_path
 }
+
+resource "aws_iam_user" "tejas" {
+  name = var.this_aws_iam_user_path_tejas.a[2]
+  path = var.this_aws_iam_user_path_tejas.c
+}
